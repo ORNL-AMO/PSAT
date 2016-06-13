d7cb1c49403989f5daf18f0bff76d052e128d20e
Noah Rosser <noah@codesimple.consulting>
2016-06-13 15:59:14 -0400

    $

--- app/index.js
+++ app/index.js
@@ -2,12 +2,12 @@ Electron = require('electron')
 App = Electron.app
 
 App.on('ready', function() {
-  Win = new Electron.BrowserWindow({width: 1280, height: 1024})
-  Win.loadURL(`file://${__dirname}/index.html`)
+  Win = new Electron.BrowserWindow({width: 1280, height: 1024});
+  Win.loadURL(`file://${__dirname}/index.html`);
 })
 
 App.on('window-all-closed', function () {
-    App.quit()
+    App.quit();
 })
 
 
