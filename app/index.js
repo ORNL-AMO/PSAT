c34d66a7a78bf7d17e81a524d843335268fde89f
Noah Rosser <noah@codesimple.consulting>
2016-06-13 20:31:46 -0400

    d

--- app/index.js
+++ app/index.js
@@ -1,6 +1,9 @@
 Electron = require('electron')
 App = Electron.app
 
+//rm if prod
+require('electron-reload')(__dirname);
+
 App.on('ready', function() {
   Win = new Electron.BrowserWindow({width: 1280, height: 1024});
   Win.loadURL(`file://${__dirname}/index.html`);
