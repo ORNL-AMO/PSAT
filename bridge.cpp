6a2f3bf30ec1bb711b261540ca03f8e2b4049185
Noah Rosser <noah@codesimple.consulting>
2016-08-26 08:36:55 -0400

    no message

--- bridge.cpp
+++ bridge.cpp
@@ -43,30 +43,32 @@ void Results(const FunctionCallbackInfo<Value>& args) {
   inp = args[0]->ToObject();
 
   set({
-    (new PumpEfficiency(get("specific_gravity"),get("flow"),get("head"),0))->calculate(),
-    (new OptimalPumpEfficiency(static_cast<Pump::Style>(get("style")),get("pump_rpm"),get("viscosity"),get("stages"),get("flow"),get("head"),static_cast<Pump::Speed>(!get("speed"))))->calculate(),
-    (new MotorRatedPower(get("power_rating")))->calculate(),
-    (new OptimalMotorRatedPower(0,get("margin")))->calculate(),
-    (new MotorShaftPower(0,0))->calculate(), 
-    (new OptimalMotorShaftPower(0,Pump::Drive::DIRECT_DRIVE))->calculate(),
-    (new PumpShaftPower(0,static_cast<Pump::Drive>(get("drive"))))->calculate(),
-    (new OptimalPumpShaftPower(get("flow"),get("head"),get("specific_gravity"),0))->calculate(),
-    (new MotorEfficiency(get("line"),get("rpm"),static_cast<Motor::EfficiencyClass>(get("efficiency_class")),get("power_rating"),static_cast<FieldData::LoadEstimationMethod>(get("load_method")),0,0,get("field_voltage")))->calculate(),
-    (new OptimalMotorEfficiency(0,0))->calculate(),
-    (new MotorPowerFactor(get("line"),get("rpm"),static_cast<Motor::EfficiencyClass>(get("efficiency_class")),get("power_rating"),static_cast<FieldData::LoadEstimationMethod>(get("load_method")),0,0,get("field_voltage")))->calculate(),
-    (new OptimalMotorPowerFactor(0,0))->calculate(),
-    (new MotorCurrent(0,0,get("field_voltage")))->calculate(),
-    (new OptimalMotorCurrent(0,get("field_voltage")))->calculate(),
-    (new MotorPower(0,0,0,get("field_voltage")))->calculate(),
-    (new OptimalMotorPower(0,0))->calculate(),
-    (new AnnualEnergy(0,get("fraction")))->calculate(),
-    (new AnnualEnergy(0,get("fraction")))->calculate(),
-    (new AnnualCost(0,0))->calculate(),
-    (new AnnualCost(0,0))->calculate(),
+    (new PumpEfficiency(get("specific_gravity"),get("flow"),get("head"),0))->calculate(),//pumpShaftPower
+    (new OptimalPumpEfficiency(static_cast<Pump::Style>(get("style")),get("pump_rated_speed"),get("viscosity"),get("stages"),get("flow"),get("head"),static_cast<Pump::Speed>(!get("speed"))))->calculate(),//
+    (new MotorRatedPower(get("motor_rated_power")))->calculate(),//INPUT, why exist?
+    (new OptimalMotorRatedPower(0,get("margin")))->calculate(),//motorshaftpower
+    (new MotorShaftPower(0,0))->calculate(),//motor eff, motor power (sometimes inp? sometimes calc) 
+    (new OptimalMotorShaftPower(0,static_cast<Pump::Drive>(get("drive"))))->calculate(),//pumpshaftpower
+    (new PumpShaftPower(0,static_cast<Pump::Drive>(get("drive"))))->calculate(),//motorshaftpower
+    (new OptimalPumpShaftPower(get("flow"),get("head"),get("specific_gravity"),0))->calculate(),//pumpeff
+    (new MotorEfficiency(get("line"),get("motor_rated_speed"),static_cast<Motor::EfficiencyClass>(get("efficiency_class")),get("motor_rated_power"),
+      static_cast<FieldData::LoadEstimationMethod>(get("load_method")),0,0,get("field_voltage")))->calculate(),//motorKwh??, motor amps
+    (new OptimalMotorEfficiency(get("motor_rated_power"),0))->calculate(),//motor shaft power
+    (new MotorPowerFactor(get("line"),get("rpm"),static_cast<Motor::EfficiencyClass>(get("efficiency_class")),get("power_rating"),
+      static_cast<FieldData::LoadEstimationMethod>(get("load_method")),0,0,get("field_voltage")))->calculate(),//motor kwh,motor a
+    (new OptimalMotorPowerFactor(get("motor_rated_power"),0))->calculate(),//motor power
+    (new MotorCurrent(0,0,get("field_voltage")))->calculate(),//motor a, motor power
+    (new OptimalMotorCurrent(0,get("field_voltage")))->calculate(),//motor power
+    (new MotorPower(0,0,0,get("field_voltage")))->calculate(),//motor power (makes no sense, it IS motor power), motor a, motorpf 
+    (new OptimalMotorPower(0,0))->calculate(),//motorshaftpower, motor eff
+    (new AnnualEnergy(0,get("fraction")))->calculate(),//motorpower
+    (new AnnualEnergy(0,get("fraction")))->calculate(),//motorpower
+    (new AnnualCost(0,get("cost")))->calculate(),//ex ann energy
+    (new AnnualCost(0,get("cost")))->calculate(),//opt ann energy
     -1,
-    (new AnnualSavingsPotential(0,0))->calculate(),
+    (new AnnualSavingsPotential(0,0))->calculate(),//ex an cost, opt an cost
     -1,
-    (new OptimizationRating(0,0))->calculate()
+    (new OptimizationRating(0,0))->calculate()//ex an cost, opt an cost
   });
   args.GetReturnValue().Set(r);
 }
