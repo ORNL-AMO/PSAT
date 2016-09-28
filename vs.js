8a0cdcd7d7d66a236acff8a8df46ccc5a9599e10
Noah Rosser <noah@codesimple.consulting>
2016-09-28 14:48:28 -0400

    no message

--- vs.js
+++ vs.js
@@ -1,4 +1,5 @@
 console.log("node",process.versions.node);
 console.log("electron",process.versions.electron);
 console.log("chrome",process.versions.chrome);
+console.log("v8",process.versions.v8);
 process.exit(0);
---
+++ win.cmd
@@ -0,0 +1,2 @@
+set npm_config_target=1.2.9
+npm install
