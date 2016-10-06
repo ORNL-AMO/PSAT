57759ecedb4b6be8851d692328bb684bbb2c06c6
Noah Rosser <noah@codesimple.consulting>
2016-10-06 12:08:43 -0400

    no message

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 Electron = require('electron')
 App = Electron.app
 
-//rm if prod
-require('electron-reload')(__dirname);
+//require('electron-reload')(__dirname);
 
 App.on('ready', function() {
   Win = new Electron.BrowserWindow({width: 800, height: 600});
@@ -13,5 +12,3 @@ App.on('window-all-closed', function () {
     App.quit();
 })
 
-// const bridge = require('./build/Release/bridge');
-// console.log(bridge.pumpEff());
