727b3c5cb893f6406474090f5c26d400c08a38ac
Noah Rosser <noah@codesimple.consulting>
2016-10-30 16:07:30 -0400

    no message

--- bridge.cpp
+++ bridge.cpp
@@ -135,7 +135,7 @@ void Nema(const FunctionCallbackInfo<Value>& args) {
   Setup(args);
   
   args.GetReturnValue().Set(
-    MotorEfficiency(line(),Get("motor_rated_speed"),effCls(),0,200,1).calculate()
+    MotorEfficiency(line(),Get("motor_rated_speed"),effCls(),Get("efficiency"),200,1).calculate()
   );
 }
 
