051a8c065ea9cc2d57a3bd2fef9cbd4c60ecbd5d
Noah Rosser <noah@codesimple.consulting>
2016-11-15 11:08:09 -0500

    no message

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ Electron = require('electron')
 App = Electron.app
 
 App.on('ready', function() {
-  Win = new Electron.BrowserWindow({width: 800, height: 600});
-  Win.loadURL(`file://${__dirname}/index.html`);
+	Win = new Electron.BrowserWindow({width: 800, height: 600});
+	Win.loadURL(`file://${__dirname}/index.html`);
 })
 
 App.on('window-all-closed', function () {
-    App.quit();
+		App.quit();
 })
 
